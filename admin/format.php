f06e4be8157f14bb7dedc8b358acc293b201f12f
Huy <118819624+HoHuuHuy285@users.noreply.github.com>
2022-12-22 23:29:54 +0700

    Update code

---
+++ admin/format.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Format Class ( Giới hạn ký tự lại , cắt ngắn văn bản )
+ */
+
+ class Format{
+    public function formatDate($date){
+        return date('F j, Y, g:i a', strtotime($date));
+    }
+
+    public function textShorten($text, $limit = 480){
+        $text = $text." ";
+        $text = substr($text, 0, $limit);
+        $text = substr($text, 0, strrpos($text, ' '));
+        $text = $text.'.....';
+        return $text;
+    }
+
+
+    public function validation($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
+    public function title(){
+        $path = $_SERVER['SCRIPT_FILENAME'];
+        $title = basename($path, '.php');
+        if($title == 'index'){
+            $title = 'home';
+        } elseif ($title == 'contact') {
+            $title = 'contact';
+        }
+        return $title = unfirst($title);
+    }
+
+
+
+ }
+
+ ?>
